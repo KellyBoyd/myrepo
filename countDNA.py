6fe05d7c8aa070a706eb323b86c4fc796ad4595a
KellyBoyd <kboyd3@luc.edu>
2016-02-22 15:55:54 -0600

    Update countDNA.py

--- countDNA.py
+++ countDNA.py
@@ -5,9 +5,7 @@ s = infile.readline() #store read lines as s
 from collections import Counter #python module that counts repeats of data
 counter = Counter(s) #count repetitions in s
 f = open("CountingDNAoutput.txt", 'w') #create file to write to 
-a = (counter['A']) #count individual bases
-c = (counter['C'])
-g = (counter['G'])
-t = (counter['T'])
-output =  (str(a) + ' ' + str(c) + ' ' + str(g) + ' ' + str(t)) #convert int to string output
-f.write(output) #print output to textfile 
+for n in ["A", "C", "G", "T"]:
+  print s.count(n)
+#output =  (str(a) + ' ' + str(c) + ' ' + str(g) + ' ' + str(t)) #convert int to string output
+#f.write(output) #print output to textfile 
